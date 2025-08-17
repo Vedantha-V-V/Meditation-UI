b59661d981250be9ac27a8a3b6deb50e945eb464
Vedantha-V-V <vernekar.vedantha2004@gmail.com>
2025-08-17 12:55:10 +0530

    First Commit

---
+++ app/src/main/java/com/example/meditationui/BottomMenuContent.kt
@@ -0,0 +1,5 @@
+package com.example.meditationui
+
+import androidx.annotation.DrawableRes
+
+data class BottomMenuContent(val title:String,@DrawableRes val iconId: Int)
---
+++ app/src/main/java/com/example/meditationui/ui/theme/Type.kt
@@ -0,0 +1,50 @@
+package com.example.meditationui.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+
+//val gothicA1 = FontFamily(
+//    listOf(
+//        Font(R.font.gothica1_regular, FontWeight.Normal),
+//        Font(R.font.gothica1_medium, FontWeight.Medium),
+//        Font(R.font.gothica1_semibold, FontWeight.SemiBold),
+//        Font(R.font.gothica1_bold, FontWeight.Bold),
+//        Font(R.font.gothica1_black, FontWeight.Black),
+//    )
+//)
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    titleLarge = TextStyle(
+        color = TextWhite,
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp,
+        letterSpacing = 0.sp
+    ),
+    titleMedium = TextStyle(
+        color = TextWhite,
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    )
+)
